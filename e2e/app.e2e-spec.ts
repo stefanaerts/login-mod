88a1114afc5844ce1e4378bebdd2c139d03a022a
Stefan Aerts <aerts.stefan@icloud.com>
2016-09-04 22:26:42 +0200

    initial commit with some changes to package.json, settings.json, typings added

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { TestcliwebpackPage } from './app.po';
+
+describe('testcliwebpack App', function() {
+  let page: TestcliwebpackPage;
+
+  beforeEach(() => {
+    page = new TestcliwebpackPage();
+  });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
+  });
+});
